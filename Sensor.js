e148e6ff2d101034d2f5836030838896bbc7f087
Remxin <remxin7@gmail.com>
2023-04-09 11:03:54 +0200

    sensor bug solved

--- Sensor.js
+++ Sensor.js
@@ -37,16 +37,19 @@ export class Sensor {
             if (touch) touches.push(touch)
         }
 
-        for (let j = 0; j < traffic.length; j++) {
-            const poly = traffic[j].polygon
-            const touch = getIntersection(
-                ray[0],
-                ray[1],
-                poly[j],
-                poly[(j+1)%poly.length]
-            )
-
-            if (touch) touches.push(touch)
+        for(let i=0;i<traffic.length;i++){
+            const poly=traffic[i].polygon;
+            for(let j=0;j<poly.length;j++){
+                const value=getIntersection(
+                    ray[0],
+                    ray[1],
+                    poly[j],
+                    poly[(j+1)%poly.length]
+                );
+                if(value){
+                    touches.push(value);
+                }
+            }
         }
 
         if (touches.length === 0) return null
